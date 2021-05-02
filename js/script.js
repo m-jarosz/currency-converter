cca50b481c106c9394abd6996bd17c8f3122c530
Mariusz Jarosz <m.jarosz82@gmail.com>
2021-05-02 14:55:40 +0200

    let -> const replaced

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 {
-    let form = document.querySelector('.js-form');
-    let amountOfPLN = document.querySelector(".js-inputAmount");
-    let currencyOfBuy = document.querySelector(".js-currencyBuy");
-    let result = document.querySelector(".js-result");
-    let resultButton = document.querySelector(".js-resultButton");
-    let resetButton = document.querySelector(".js-resetButton");
-    let euro = 4.56;
-    let gbp = 5.23;
-    let usd = 3.77;
+    const form = document.querySelector('.js-form');
+    const amountOfPLN = document.querySelector(".js-inputAmount");
+    const currencyOfBuy = document.querySelector(".js-currencyBuy");
+    const result = document.querySelector(".js-result");
+    const resultButton = document.querySelector(".js-resultButton");
+    const resetButton = document.querySelector(".js-resetButton");
+    const euro = 4.56;
+    const gbp = 5.23;
+    const usd = 3.77;
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
